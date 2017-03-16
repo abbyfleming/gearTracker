4a324182d7a31e6531bc027bd820b1bcf3dfe0bf
Abby Moore <abbyleighanne@gmail.com>
2017-03-16 16:00:02 -0500

    track checkboxes

--- gearTracker_website/tracker_app/views/event_has_gear_view.py
+++ gearTracker_website/tracker_app/views/event_has_gear_view.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponseRedirect
 
+from tracker_app.models import Customer
 from tracker_app.models import Event
+from tracker_app.models import LensModel
+from tracker_app.models import CameraModel
+from tracker_app.models import PhotoshootHasGear
+
+
 
 
 class EventHasGearView(TemplateView):
@@ -19,24 +25,57 @@ class EventHasGearView(TemplateView):
     def get(self, request):
 
         self.all_events = Event.objects.all()
-
+        self.all_lens = LensModel.objects.all()
+        self.all_camera = CameraModel.objects.all()
+        
         return render(
-            request, 'create_event_gear.html',
-            {'event': self.all_events,}
-            )
+            request, 'create_event_gear.html', {
+            'event': self.all_events,
+            'lens': self.all_lens,
+            'camera': self.all_camera,
+            })
 
 
     def post(self, request):
-        data = request.POST
+        # data = request.POST
+        # print("*****data*****", data)
+        
+        lens = request.POST.getlist('lens')
+        print("*****lens*****", lens)
+        
+        camera = request.POST.getlist('camera')
+        print("*****camera*****", camera)
+        
+        # Fetch the data from the form
+        # event = data['event']
+        
+        for l in lens:
+            print("***lens index****", l)
 
-        name = data['event']
+        for c in camera:
+            print("***camera index***", c)
 
-        create_event = Event.objects.create(
-            name=name,
-            )
+        # print("Type of lens", type(lens))
+
+        # print("***first item in lens", data['lens'][0])
+
+        #loop over lens and camera to pull the objects out.
+        
+
+        # Find the FK information
+        # customer = Customer.objects.get(user=request.user.pk)
+        # event = Event.objects.get(pk=e)
+        # lens = LensModel.objects.get(pk=l)
+        # camera = CameraModel.objects.get(pk=c)
+          
+        # create_event_has_gear = PhotoshootHasGear.objects.create(
+        #     event = event,
+        #     camera = camera,
+        #     lens = lens,
+        #     )
 
         # Redirect to same page
-        return HttpResponseRedirect("/create_event_gear")
+        return HttpResponseRedirect("/event-gear")
 
 
     
