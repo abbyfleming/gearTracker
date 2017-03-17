273460dc24b9e832367c62e8bae452c838dff767
Abby Moore <abbyleighanne@gmail.com>
2017-03-17 14:40:58 -0500

    add photoshoot

--- gearTracker_website/tracker_app/views/photoshoot_view.py
+++ gearTracker_website/tracker_app/views/photoshoot_view.py
@@ -4,15 +4,18 @@ from django.shortcuts import render
 from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponseRedirect
 
-from tracker_app.models import LensMake
+
 from tracker_app.models import LensModel
-from tracker_app.models import CameraMake
+from tracker_app.models import CameraModel
 from tracker_app.models import Customer
+from tracker_app.models import PhotoshootHasGear
+from tracker_app.models import Event
+from tracker_app.models import Photoshoot
 
 
 class PhotoShootView(TemplateView):
     """
-    Purpose: LENS MODEL: Nikon Nikkor 70-200 2.8
+    Purpose: Bring gear to a shoot!
     Methods: post, get
     """
 
@@ -20,37 +23,39 @@ class PhotoShootView(TemplateView):
 
     def get(self, request):
 
-        # Fetch the camera brands
-        self.all_lens_makes = LensMake.objects.all()
-        self.all_camera_makes = CameraMake.objects.all()
-        
+        # Fetch the event
+        self.event = Event.objects.all()
+ 
         return render(
             request, 'photoshoot.html',
-            {'lens_make': self.all_lens_makes,
-            'camera_make':self.all_camera_makes}
+            {'event': self.event}
             )
 
 
     def post(self, request):
         data = request.POST
+        print("*****data*****", data)
 
         # Fetch the data from the Form
-        make = data['lens_list']
-        mount = data['camera_list']
-        date = data['purchase_date']
-        min_focal = data['min_focal']
-        max_focal = data['max_focal']
-        aperature = data['aperature']
-        purchase = data['purchase_date']
-    
-        # Find the values of the FK
-        customer = Customer.objects.get(user=request.user.pk) 
-        camera = CameraMake.objects.get(pk=mount)    
-        lens = LensMake.objects.get(pk=make)
+        client_name = data['client_name']
+        location = data['location']
+        date =  data['date']
+        event = data['event']
 
+        # Find the values of the FK
+        current_user = Customer.objects.get(user=request.user.pk)
+        current_gear = PhotoshootHasGear.objects.get(event=event)
+
+        create_photoshoot = Photoshoot.objects.create(
+            customer = current_user,
+            gear = current_gear,
+            client_name = client_name,
+            location = location,
+            date = date,
+            )
 
         # Redirect to same page
-        return HttpResponseRedirect("/add-lens")
+        return HttpResponseRedirect("/photoshoot")
 
 
     
