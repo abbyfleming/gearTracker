23d3cb6fa2fa03adb06c1d0febdbae38b30dc65c
Abby Moore <abbyleighanne@gmail.com>
2017-03-17 15:44:20 -0500

    initial data shows

--- gearTracker_website/tracker_app/views/index_view.py
+++ gearTracker_website/tracker_app/views/index_view.py
@@ -1,6 +1,9 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
+from tracker_app.models import Event
+from tracker_app.models import CameraModel
+from tracker_app.models import LensModel
 
 class Index(TemplateView):
   '''
@@ -9,3 +12,17 @@ class Index(TemplateView):
   Author:
   '''
   template_name = "index.html"	
+
+  def get(self, request):
+
+    self.all_event = Event.objects.all()
+    self.all_camera = CameraModel.objects.all()
+    self.all_lens = LensModel.objects.all()
+
+    return render(
+        request, 'index.html',{
+        'event': self.all_event,
+        'camera': self.all_camera,
+        'lens': self.all_lens,
+        }
+        )
